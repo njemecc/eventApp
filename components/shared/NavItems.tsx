472466fda2fe93b9e69eb4f9a3ad95303e213474
brani <branislannjemec@gmail.com>
2024-01-11 23:03:21 +0100

    hero section

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -6,17 +6,18 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 const NavItems = () => {
-  const pathname = usePathname();
+  const pathName = usePathname();
 
   return (
     <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
       {headerLinks.map((link) => {
-        const isActive = pathname === link.route;
+        const isActive = pathName === link.route;
 
         return (
           <li
             key={link.route}
-            className={`${
+            className={`
+            ${
               isActive && "text-primary-500"
             } flex-center p-medium-16 whitespace-nowrap`}
           >
