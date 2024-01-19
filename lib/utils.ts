5285e861fda53de6088ec0167d681dc43c10834f
brani <branislannjemec@gmail.com>
2024-01-19 17:47:57 +0100

    install qs

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { UrlQueryParams, RemoveUrlQueryParams } from "@/types";
-
+import qs from "query-string";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
