5be3504d0271f93cb69415f4af1c3c352f8dd964
brani <branislannjemec@gmail.com>
2024-01-28 11:00:44 +0100

    create update page

---
+++ app/(root)/events/[id]/update/page.tsx
@@ -0,0 +1,30 @@
+import Categorylist from "@/components/shared/Categorylist";
+import EventForm from "@/components/shared/EventForm";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const UpdateEvent = () => {
+  //clerk sesija
+  const { sessionClaims } = auth();
+
+  console.log("e");
+
+  const userId = sessionClaims?.userId as string;
+
+  console.log(userId);
+
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left">
+          Update Event
+        </h3>
+      </section>{" "}
+      <div className="wrapper my-8">
+        <EventForm userId={userId} type="Update" />
+      </div>
+    </>
+  );
+};
+
+export default UpdateEvent;
