9f2fc978214066bf5872664afcd66ec0f55656ae
brani <branislannjemec@gmail.com>
2024-01-30 21:53:27 +0100

    add My tickets feature

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -3,14 +3,22 @@ import { getEventsByUser } from "@/lib/actions/event.actions";
 import { auth } from "@clerk/nextjs";
 import Collection from "@/components/shared/Collection";
 import Link from "next/link";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/mongodb/database/models/order.model";
+import { SearchParamProps } from "@/types";
 
-const ProfilePage = async () => {
+const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const { sessionClaims } = auth();
+
+  const ordersPage = Number(searchParams?.ordersPage) || 1;
+  const eventsPage = Number(searchParams?.eventsPage) || 1;
 
   const userId = sessionClaims?.userId as string;
 
-  const organizedEvents = await getEventsByUser({ userId, page: 1 });
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage });
 
+  const orders = await getOrdersByUser({ userId, page: ordersPage });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -23,16 +31,16 @@ const ProfilePage = async () => {
       </section>
 
       <section className="wrapper my-8">
-        {/* <Collection
-          data={events?.data}
+        <Collection
+          data={orderedEvents}
           emptyTitle="No Events Tickets Purchased Yet"
           emptyStateSubtext="No worries - plenty of exciting events to explore !"
           collectionType="My_Tickets"
           limit={3}
-          page={page}
+          page={ordersPage}
           urlParamName="ordersPage"
-          totalPages={2}
-        /> */}
+          totalPages={orders?.totalPages}
+        />
       </section>
 
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -50,9 +58,9 @@ const ProfilePage = async () => {
           emptyStateSubtext="Go create some now"
           collectionType="Events_Organized"
           limit={6}
-          page={1}
+          page={eventsPage}
           urlParamName="eventsPage"
-          totalPages={2}
+          totalPages={organizedEvents?.totalPages}
         />
       </section>
     </>
